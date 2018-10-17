49ca2601f5af577b6895e4756c7fc64d2cd33dd0
jshalpin <43503553+jshalpin@users.noreply.github.com>
2018-10-16 20:03:42 -0700

    Add files via upload

---
+++ TIVA C/Lab 9/Task01/Task01.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/fpu.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
+
+//hardcode Pi value
+#ifndef M_PI
+#define M_PI                    3.14159265358979323846
+#endif
+
+//define amount of data point for wave
+#define SERIES_LENGTH 100
+
+float gSeriesData[SERIES_LENGTH];
+
+int32_t i32DataCount = 0;
+
+int main(void)
+{
+    float fRadians;
+
+    //enable Lazy Stack to reduce latency
+    FPULazyStackingEnable();
+    //enable FPU
+    FPUEnable();
+
+    //set clock rate for board
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    //calculate radians used for waveform
+    fRadians = ((2 * M_PI) / SERIES_LENGTH);
+
+    //do while i32DataCount is less than SERIES_LENGTH
+    while(i32DataCount < SERIES_LENGTH)
+    {
+    	//get the waveform value
+        gSeriesData[i32DataCount] = sinf(fRadians * i32DataCount);
+
+        //increment i32DataCount
+        i32DataCount++;
+    }
+
+    while(1)
+    {
+    }
+}
